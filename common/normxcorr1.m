33f11f7fc3cc92d0cc20f6cbd07af775a899f331
Eric Tytell <tytell@umd.edu>
2003-01-03 09:11:09 -0500

    Added basic support for same size window and search regions

--- common/normxcorr1.m
+++ common/normxcorr1.m
@@ -1,17 +1,24 @@
 function C = normxcorr1(win, search)
 
+if (length(win) == length(search)),
+	w2 = ceil(length(win)/2);
+	
+	search = shiftdim(search);
+	search = [repmat(NaN,[w2 1]); search; repmat(NaN,[w2 1])];
+end;
+
 win = shiftdim(win);
 
-winmean = mean(win);
+winmean = nanmean(win);
 win = win - winmean;
-winvar = sum(win.^2);
+winvar = nansum(win.^2);
 
 search = shiftdim(search);
 S = im2col(search, [length(win) 1], 'sliding');
 
-Smean = mean(S);
+Smean = nanmean(S);
 S = S - repmat(Smean, [size(S,1) 1]);
-Svar = sum(S.^2);
+Svar = nansum(S.^2);
 
-C = sum(repmat(win, [1 size(S,2)]) .* S)  ./  sqrt(winvar .* Svar);
+C = nansum(repmat(win, [1 size(S,2)]) .* S)  ./  sqrt(winvar .* Svar);
 
