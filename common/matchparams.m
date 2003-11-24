01e3d76ea1c7df97ad69561854b2e0c7fbcf6f78
Eric Tytell <tytell@umd.edu>
2003-11-24 10:53:56 -0500

    Initial, partially working version.  Parses different combinations of parameters based on a set of specifications.

---
+++ common/matchparams.m
@@ -0,0 +1,137 @@
+function tp = matchparams(p, req, opt)
+
+nd = 0;
+for i = 1:length(p),
+    nd = max(nd,ndims(p{i}));
+    sz(1:ndims(p{i}),i) = size(p{i})';
+end;
+
+for i = 1:length(req),
+    r1 = req{i};
+
+    [match(i),len(i)] = matchparamtype(p,sz,req{i}, []);
+end;
+
+if (sum(match) == 0),
+    tp = 0;
+elseif (sum(match) == 1),
+    tp = find(match == 1);
+else
+    [q,tp] = max(len.*match);
+end;
+
+% -------------------------------------
+function [match,len,szlist] = matchparamtype(p,sz, r1, szlist)
+
+match = 1;
+len = 0;
+for j = 1:length(r1),
+    pspec = r1{j};
+
+    if (iscell(pspec{2})),
+        k = 1;
+        m0 = 0;
+        pspec2 = pspec;
+        while (~m0 & (k <= length(pspec{2}))),
+            pspec2{2} = pspec{2}{k};
+            [m0,l,szlist] = matchparamtype(p(j),sz(:,j), {pspec2}, ...
+                                           szlist);
+            k = k+1;
+        end;
+        if (~m0),
+            match = 0;
+            break;
+        end;
+    else
+        done = 0;
+        switch pspec{2},
+         case {'*', '+'},
+          r2 = pspec([1 3:end]);
+          [m0,l,szlist] = matchparamtype(p(j:end),sz(:,j:end), {r2}, ...
+                                         szlist);
+          if (~m0 & (pspec{2} == '+')),
+              match = 0;
+              break;
+          end;
+
+          k = j+1;
+          while (m0 & (k <= length(p))),
+              [m0,l,szlist] = matchparamtype(p(k:end),sz(:,k:end), ...
+                                             {r2}, szlist);
+              k = k+1;
+          end;
+          len = k-1;
+          done = 1;
+
+         case 'cellstr',
+          if (~iscellstr(p{j})),
+              match = 0;
+              break;
+          end;
+          matchsz = pspec{3};
+
+         case 'cell',
+          if (~iscell(p{j})),
+              match = 0;
+              break;
+          end;
+          if (iscell(pspec{3})),
+              p2 = p{j};
+
+              for k = 1:length(p2),
+                  sz2(1:ndims(p2{k}),k) = size(p2{k})';
+              end;
+              [match,l,szlist] = matchparamtype(p2,sz2, {pspec{3}}, szlist);
+              if (~match),
+                  break;
+              else
+                  done = 1;
+              end;
+          else,
+              matchsz = pspec{3};
+          end;
+         otherwise,
+          if (~isa(p{j},pspec{2})),
+              match = 0;
+              break;
+          end;
+          matchsz = pspec{3};
+        end;
+
+        if (~done),
+            matchsz = shiftdim(matchsz);
+            if (length(matchsz) == 1),
+                p{j} = shiftdim(p{j});
+                if (size(p{j},2) ~= 1),
+                    match = 0;
+                    break
+                end;
+                sz(:,j) = size(p{j})';
+                matchsz(2) = -1;
+            end;
+
+            if (ndims(p{j}) ~= length(matchsz)),
+                match = 0;
+                break;
+            else
+                if (max(matchsz) > length(szlist)),
+                    szlist(max(matchsz),1) = 0;
+                end;
+
+                k = find(matchsz > 0);
+                checksz(k,1) = szlist(matchsz(k));
+                checksz(matchsz < 0) = -matchsz(matchsz < 0);
+                checksz(checksz == 0) = sz(checksz == 0,j);
+
+                if (any(checksz ~= sz(:,j))),
+                    match = 0;
+                    break;
+                end;
+                szlist(matchsz(k)) = sz(k,j);
+            end;
+        end;
+    end;
+
+    len = len+1;
+end;
+
