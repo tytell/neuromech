b61092330b042938d95e24327a20dbf39f1f7217
Eric Tytell <tytell@umd.edu>
2004-10-20 16:39:11 -0400

    Calculates combinations for vectors, since Matlab's nchoosek can't handle vector inputs.

---
+++ common/combination.m
@@ -0,0 +1,25 @@
+function C = combination(n,k)
+
+if (prod(size(n)) == 1),
+    sz = size(k);
+    n = repmat(n,sz);
+elseif (prod(size(k)) == 1),
+    sz = size(n);
+    k = repmat(k,sz);
+elseif ((ndims(n) == ndims(k)) & all(size(n) == size(k))),
+    sz = size(n);
+else
+    error('n and k need to be the same size or scalar.');
+end;
+
+N = prod(sz);
+
+n = n(:);
+k = k(:);
+
+for i = 1:N,
+    C(i) = prod((n(i)-k(i)+1):n(i))/factorial(k(i));
+end;
+
+C = reshape(C,sz);
+
