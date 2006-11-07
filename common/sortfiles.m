ff470dd86291198ed31a5d339a5e948aafff1b9f
Eric Tytell <tytell@umd.edu>
2006-11-07 16:06:32 -0500

    Added fillmat.m, which fills in NaN values in a matrix with the previous non-NaN value.  Also a bug fix in sortfiles.  superyyplot is now obsolete and replaced by overlayplot.

--- common/sortfiles.m
+++ common/sortfiles.m
@@ -6,26 +6,28 @@ function [fn,ind] = sortfiles(fn)
 % Note: This is probably rather slow for long lists of unnumbered
 % files.
 
-[fn,ind] = sort(fn);
-for i = 1:length(fn),
-    [rind,q,tok] = regexp(fn{i},'(.*[^0-9])([0-9]+)[^0-9]*');
-    if (~isempty(rind)),
-        tok = tok{1};
-        base{i} = fn{i}(tok(1,1):tok(1,2));
-        num(i) = str2num(fn{i}(tok(2,1):tok(2,2)));
-    else
-        base{i} = fn{i};
+if (~isempty(fn)),
+    [fn,ind] = sort(fn);
+    for i = 1:length(fn),
+        [rind,q,tok] = regexp(fn{i},'(.*[^0-9])([0-9]+)[^0-9]*');
+        if (~isempty(rind)),
+            tok = tok{1};
+            base{i} = fn{i}(tok(1,1):tok(1,2));
+            num(i) = str2num(fn{i}(tok(2,1):tok(2,2)));
+        else
+            base{i} = fn{i};
+        end;
     end;
-end;
 
-[basenames,q,baseind] = unique(base);
+    [basenames,q,baseind] = unique(base);
 
-if (length(basenames) ~= length(fn)),
-    for i = 1:length(basenames),
-        k = find(baseind == i);
-        [a,jig] = sort(num(k));
-        fn(k) = fn(k(jig));
-        ind(k) = ind(k(jig));
+    if (length(basenames) ~= length(fn)),
+        for i = 1:length(basenames),
+            k = find(baseind == i);
+            [a,jig] = sort(num(k));
+            fn(k) = fn(k(jig));
+            ind(k) = ind(k(jig));
+        end;
     end;
 end;
 
