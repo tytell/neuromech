afd00fe204c0092681eb3831dfd34f1277743aea
Eric Tytell <tytell@umd.edu>
2008-09-03 06:52:34 -0400

    Sets up aliases for raw data on external hard drives

---
+++ common/aliasrawdata.m
@@ -0,0 +1,48 @@
+function aliasrawdata
+
+if (~ismac && ~isunix),
+    error('Aliases do not work well under Windows');
+end;
+
+[fn,pn] = uigetfile('*','Select raw data files','/Volumes', ...
+    'MultiSelect','on');
+if (~iscell(fn)),
+    if (ischar(fn)),
+        fn = {fn};
+    elseif (isnumeric(fn) && (fn == 0)),
+        return;
+    end;
+end;
+
+outdir = uigetdir('','Select output directory');
+if (~ischar(outdir) && (outdir == 0)),               % canceled
+    return;
+end;
+
+if (outdir(end) ~= '/'),
+    outdir(end+1) = '/';
+end;
+
+overwriteall = false;
+for i = 1:length(fn),
+    fn1 = fullfile(pn,fn{i});
+    
+    cmd = ['ln -sf ' fn1 ' ' outdir];
+    if (~overwriteall && exist(fullfile(outdir,fn{i}),'file')),
+        butt = questdlg(sprintf('Alias to %s exists.  Overwrite?',fn{i}), ...
+            'Overwrite?','This file only','All files','Skip','Skip');
+        
+        switch butt,
+            case 'All files',
+                overwriteall = true;
+            case 'Skip',
+                cmd = '';
+        end;
+    end;
+        
+    if (~isempty(cmd)),
+        system(cmd);
+    end;
+end;
+
+    
