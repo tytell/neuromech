22cb3a8098bfbcf95b629635319afd95a7559b60
Eric Tytell <tytell@jhu.edu>
2011-06-23 12:57:09 -0400

    Also can take a set of datafiles as inputs and saves SHA1 hash values for the datafiles.

--- common/savehgrev.m
+++ common/savehgrev.m
@@ -1,16 +1,19 @@
 function out = savehgrev(out, varargin)
 %SAVEHGREV  Produces an HGREV structure with the current changeset
-%   savehgrev(outfile, options...)
+%   savehgrev(outfile, [repo], options...)
 %     or
-%   outstruct = savehgrev(outstruct, options...)
+%   outstruct = savehgrev(outstruct, [repo], options...)
 %
 %  The HGREV structure contains the revision number, the changeset hash,
 %  and the description, branch, and date information.  Useful to replicate
-%  calculations that produced a particular data file.
+%  calculations that produced a particular data file.  Takes an optional
+%  parameter, repo, that specifies the repository directory.
 %
 % OPTIONS
 % General options:
 %   'caller' - Identifies the calling function that generates a data file.
+%   'datafile' - Identifies the input data file to the calling function
+%     (ie, the raw data).
 %
 % Warning options:  Each of these can have the value 'error','warning', or
 %      'none', and produces an error, warning, or nothing.
@@ -32,10 +35,32 @@ opt.uncommitted = 'warning';
 opt.untracked = 'none';
 opt.nottip = 'warning';
 opt.caller = '';
+opt.datafile = '';
+opt.savemainrepo = true;
+opt.mainrepolocation = '/Users/eric/Documents/Matlab';
 
-opt = parsevarargin(opt,varargin, 2);
+if ((length(varargin) >= 1) && exist(varargin{1},'dir'))
+    repo = {'-R ',varargin{1}};
+    repoloc = varargin{1};
+    p = 2;
+else
+    repo = {};
+    repoloc = 'current directory';
+    p = 1;
+end;
 
-[s,res] = hg('summary','echo',false);
+opt = parsevarargin(opt,varargin(p:end), p+1, 'typecheck',false);
+
+if (~isempty(opt.caller) && exist(opt.caller,'file') && ...
+        isempty(dir(opt.caller)) && ~isempty(dir([opt.caller '.m'])))
+    %check for missing .m extension and add it if necessary
+    opt.caller = [opt.caller '.m'];
+end;
+    
+[s,res] = hg(repo{:},'summary','echo',false);
+if (s ~= 0)
+    error('savehgrev:hgerror','Error running hg');
+end;
 
 %check for uncommitted
 switch lower(opt.uncommitted)
@@ -51,20 +76,20 @@ end;
 if (~isempty(warnfunc))
     isuncom = false;
     if (~isempty(opt.caller))
-        [~,stat] = hg('status -A',opt.caller,'echo',false);
+        [~,stat] = hg(repo{:},'status -A',opt.caller,'echo',false);
         sttok = regexp(stat, '^([MARC!?I])', 'once','tokens');
    
         if (sttok{1} ~= 'C')
             isuncom = true;
         end;
     else
-        [~,stat] = hg('status -mard','echo',false);
+        [~,stat] = hg(repo{:},'status -mard','echo',false);
         if (~isempty(stat))
             isuncom = true;
         end;
     end;
     if (isuncom)
-        feval(warnfunc,'savehgrev:uncommitted','Files are uncommitted in the current directory.');
+        feval(warnfunc,'savehgrev:uncommitted','Files are uncommitted in %s.',repoloc);
     end;
 end;
 
@@ -80,9 +105,9 @@ switch lower(opt.untracked)
         error('savehgrev:badoption','Unrecognized value ''%s'' for untracked option', opt.untracked);
 end;
 if (~isempty(warnfunc))
-    [~,stat] = hg('status -u','echo',false);
+    [~,stat] = hg(repo{:},'status -u','echo',false);
     if (~isempty(stat))
-        feval(warnfunc,'savehgrev:untracked','Files are untracked in the current directory.');
+        feval(warnfunc,'savehgrev:untracked','Files are untracked in %s.',repoloc);
     end;
 end;
    
@@ -90,7 +115,7 @@ end;
 cstok = regexp(res, 'parent: (\d+):([0-9a-f]+)\s(\w+)?', 'tokens','once');
 currev = str2double(cstok{1});
 %get the full changeset hash
-[~,cshash] = hg('log -r ',cstok{1},' --template ''{node}''','echo',false);
+[~,cshash] = hg(repo{:},'log -r ',cstok{1},' --template ''{node}''','echo',false);
 
 %check to see if we're not at the tip
 if (~strcmp(cstok{3},'tip'))
@@ -105,13 +130,13 @@ if (~strcmp(cstok{3},'tip'))
             error('savehgrev:badoption','Unrecognized value ''%s'' for ''nottip'' option', opt.untracked);
     end;
     if (~isempty(warnfunc))
-        feval(warnfunc,'savehgrev:nottip','Current directory is not at tip revision');
+        feval(warnfunc,'savehgrev:nottip','%s is not at tip revision',repoloc);
     end;
 end;
 
-[~,branch] = hg('log -r ',cstok{1},' --template ''{branches}''','echo',false);
-[~,desc] = hg('log -r ',cstok{1},' --template ''{desc}''','echo',false);
-[~,revdate] = hg('log -r ',cstok{1},' --template ''{date|isodate}''','echo',false);
+[~,branch] = hg(repo{:},'log -r ',cstok{1},' --template ''{branches}''','echo',false);
+[~,desc] = hg(repo{:},'log -r ',cstok{1},' --template ''{desc}''','echo',false);
+[~,revdate] = hg(repo{:},'log -r ',cstok{1},' --template ''{date|isodate}''','echo',false);
 
 HGREV.rev = currev;
 HGREV.changeset = cshash;
@@ -122,10 +147,52 @@ if (~isempty(opt.caller))
     HGREV.caller = opt.caller;
 end;
 
+if (~isempty(opt.datafile))
+    if (ischar(opt.datafile))
+        datafiles = {opt.datafile};
+    else
+        datafiles = opt.datafile;
+    end;
+    for i = 1:length(datafiles),
+        dfinfo1 = dir(datafiles{i});
+        datafile1 = datafiles{i};
+        if (isempty(dfinfo1))
+            datafile1 = [datafile1 '.mat'];
+            dfinfo1 = dir(datafile1);
+            if (isempty(dfinfo1))
+                error('savehgrev:unknowndatafile','Data file %s not found',datafiles{i});
+            end;
+        end;
+        [s,dfhash1] = system(['openssl sha1 ' datafile1]);
+        
+        if (s == 0)
+            hash1 = regexp(dfhash1,'=\s*([0-9a-f]+)','tokens','once');
+            dfinfo1.sha1 = hash1{1};
+        end;
+        
+        dfinfo(i) = dfinfo1;
+    end;
+    
+    HGREV.datafiles = dfinfo;
+end;
+
+if (opt.savemainrepo)
+    opt1 = opt;
+    opt1.savemainrepo = false;
+    opt1.caller = '';
+    opt1.datafile = '';
+    kv = opt2keyvalue(opt1);
+    
+    mainrepo = savehgrev([], opt.mainrepolocation, kv{:});
+    
+    HGREV.main = mainrepo;
+    HGREV.main.location = opt.mainrepolocation;
+end;
+
 if (ischar(out) && exists(out,'file'))
     save(out,'HGREV','-append');
-elseif (isstruct(out))
-    out.HGREV = HGREV;
+elseif (isempty(out) || isstruct(out))
+    out = HGREV;
 end;
 
 
