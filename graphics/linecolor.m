f5e2719c39db21fcd8a49287e70c03db18118996
Eric Tytell <tytell@jhu.edu>
2011-12-13 13:54:58 -0500

    linecolor: Plots a line with variable color and width

---
+++ graphics/linecolor.m
@@ -0,0 +1,62 @@
+function h = linecolor(x,y,w,c,varargin)
+
+if ((numel(x) == 1) && ishandle(x) && strcmp(get(x,'Type'),'axes'))
+    ax = x;
+    [x,y,w] = deal(y,w,c);
+    if ((nargin == 5) && isnumeric(varargin{1}))
+        c = varargin{1};
+        varargin = varargin(2:end);
+    end;
+else
+    ax = gca;
+end;
+
+switch get(ax,'NextPlot'),
+ case 'replace',
+  cla(ax,'reset');
+ case 'replacechildren',
+  cla(ax);
+end;
+
+if (size(y,1) == 1)
+    y = y';
+    x = x';
+    w = w';
+    c = c';
+end;
+if (any(size(x) == 1) && (length(x) == size(y,1)))
+    x = x(:);
+    x = x(:,ones(1,size(y,2)));
+end;
+
+if (isempty(w) || (all(w(:) == 0)))
+    h = patch(x,y,c,'Parent',ax);
+    set(h,'FaceColor','none','EdgeColor','interp', varargin{:});
+else
+    dx = diff(x);
+    dy = diff(y);
+    ds = sqrt(dx.^2 + dy.^2);
+    
+    dxds = dx ./ ds;
+    dyds = dy ./ ds;
+    dxds = dxds([1:end end],:);
+    dyds = dyds([1:end end],:);
+    
+    x1 = shiftdim(x + w.*dyds, -1);
+    x2 = shiftdim(x - w.*dyds, -1);
+    y1 = shiftdim(y - w.*dxds, -1);
+    y2 = shiftdim(y + w.*dxds, -1);
+    
+    xx = cat(1,x1(1,1:end-1,:),x2(1,1:end-1,:),x2(1,2:end,:),x1(1,2:end,:));
+    yy = cat(1,y1(1,1:end-1,:),y2(1,1:end-1,:),y2(1,2:end,:),y1(1,2:end,:));
+    
+    c = c(:)';
+    c = cat(1,c([1 1],1:end-1),c([1 1],2:end));
+    
+    h = patch(xx,yy,c,'Parent',ax);
+    set(h,'FaceColor','interp', 'EdgeColor','none', varargin{:});
+end;
+
+
+
+
