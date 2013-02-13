04faf53f127f6686c96705579c19bff52b2ae513
Eric Tytell <eric.tytell@tufts.edu>
2013-02-13 15:46:05 -0500

    Fixed a bug with the Yates correction; added a table output

--- common/crosstabG.m
+++ common/crosstabG.m
@@ -2,6 +2,9 @@ function varargout = crosstabG(E,O, varargin)
 
 opt.computemarginal = true;
 opt.computeindividual = true;
+opt.rownames = {};
+opt.colnames = {};
+opt.showtable = false;
 
 sz = size(E);
 if ((nargin == 1) || ~isnumeric(O) || any(size(O) ~= sz))
@@ -55,9 +58,9 @@ if (nd == 2) && isempty(E)
 end
 
 if all(sz == 2)
-    %Yates correction
+    %Yates correction (Zar, p.505)
     corr = sign(E-O) * 0.5;
-    E = E + corr;
+    O = O + corr;
 end
 
 G = 2 * sum( sum( O .* log(O ./ E)));
@@ -66,6 +69,7 @@ P = 1 - chi2cdf(G,df);
 
 Gstat.E = E;
 Gstat.O = O;
+Gstat.G = G;
 Gstat.df = df;
 Gstat.P = P;
 
@@ -97,6 +101,35 @@ end
 if (istranspose)
     Gstat.E = Gstat.E';
     Gstat.O = Gstat.O';
+end
+
+if opt.showtable
+    fprintf('%20s %6s %6s %6s\n', '', 'G2','df','P');
+    fprintf('%20s %6.2f %6d %6.3f\n', 'independence', Gstat.G, Gstat.df, Gstat.P);
+    
+    if opt.computemarginal
+        fprintf('%20s %6.2f %6d %6.3f\n', 'rows differ', Gstat.Grow, Gstat.dfrow, Gstat.Prow);
+        fprintf('%20s %6.2f %6d %6.3f\n', 'columns differ', Gstat.Gcol, Gstat.dfcol, Gstat.Pcol);
+    end
+    if opt.computeindividual
+        for i = 1:sz(1)
+            if isempty(opt.rownames)
+                rowname1 = sprintf('row %d',i);
+            else
+                rowname1 = opt.rownames{i};
+            end
+            fprintf('%20s %6.2f %6d %6.3f\n', rowname1, Gstat.Growindiv(i), Gstat.dfrowindiv(i), Gstat.Prowindiv(i));
+        end
+        
+        for j = 1:sz(2)
+            if isempty(opt.colnames)
+                colname1 = sprintf('col %d',j);
+            else
+                colname1 = opt.colnames{j};
+            end
+            fprintf('%20s %6.2f %6d %6.3f\n', colname1, Gstat.Gcolindiv(j), Gstat.dfcolindiv(j), Gstat.Pcolindiv(j));
+        end
+    end
 end
 
 if nargout == 1
