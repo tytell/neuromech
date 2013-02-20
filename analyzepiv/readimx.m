7e473a4ea1c2ad58d528dd385aad0e78a7fe17a7
Eric Tytell <eric.tytell@tufts.edu>
2013-02-20 13:35:15 -0500

    Added readimx function from Lavision
    Includes the DLL for the MEX function

---
+++ analyzepiv/readimx.m
@@ -0,0 +1,12 @@
+%CALL:     M = readimx(S)
+%
+%FUNCTION: Reading IMX/IM7 image files (LaVision format).
+%
+%ARGUMENT: S = Matlab string of a valid filename (DaVis IMG/IMX/VEC and IM//VC7 format) 
+%
+%RETURN:   M = Matlab structure with DaVis IMX data
+%
+%Note:     View M by SHOWIMX.
+
+
+																										
