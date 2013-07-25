cd187e00864767baa126d0940ef2bc71d32abc2e
Eric Tytell <eric.tytell@tufts.edu>
2013-07-25 11:06:11 -0400

    colorby can take RGB colors too

--- graphics/colorby.m
+++ graphics/colorby.m
@@ -1,13 +1,18 @@
 function colorby(h,gp, varargin)
 
 opt.color = 'bgrmcyk';
-opt = parsevarargin(opt,varargin, 3);
+opt = parsevarargin(opt,varargin, 3, 'typecheck',false);
 
 [gpnm,~,gp] = unique(gp,'stable');
+
+col = opt.color;
+if (ischar(col))
+    col = col(:);
+end;
 
 for i = 1:length(gpnm)
     isgp = gp == i;
     
-    j = mod(i-1,length(opt.color)) + 1;
-    set(h(isgp),'Color',opt.color(j));
+    j = mod(i-1,size(col,1)) + 1;
+    set(h(isgp),'Color',col(j,:));
 end
