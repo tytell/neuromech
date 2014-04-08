2b8e3d3658ed8483d4d78cb7e973fd0ef8c8a19a
Eric Tytell <eric.tytell@tufts.edu>
2014-04-08 15:57:09 -0400

    White background

--- graphics/figureseries.m
+++ graphics/figureseries.m
@@ -3,7 +3,7 @@ function fig = figureseries(name)
 fig = findobj('Type','figure', 'Name',name);
 
 if (isempty(fig))
-    fig = figure('Name',name);
+    fig = figure('Name',name, 'Color','w');
 end
 figure(fig)
 
