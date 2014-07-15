19493b7a529b0d36cab066a9f0bce80c67938588
daniela bernales <dani.bernales@gmail.com>
2014-07-15 11:38:15 -0400

    Tests to remove common mode

--- neuro/remove_common_mode.m
+++ neuro/remove_common_mode.m
@@ -1,34 +1,29 @@
-function [rem,common,commonmag] = remove_common_mode(sig, varargin)
+function [rem,noise,noisemag] = remove_common_mode(sig, fs,goodband, varargin)
 
-opt.nfft = 512;
-opt.window = [];
-opt.noverlap = [];
-opt.coherethresh = 0.1;
+opt.order = 9;
+opt.clip = 10;
 opt = parsevarargin(opt, varargin, 2);
 
+[z,p,k] = butter(opt.order,goodband/(fs/2), 'stop');
+[sos,g] = zp2sos(z,p,k);
+
 nchan = size(sig,2);
-coh = zeros(opt.nfft,nchan*(nchan-1)/2);
-k = 1;
-for i = 1:nchan
-    for j = i+1:nchan
-        [coh(:,k),f1] = mscohere(sig(:,i),sig(:,j),hanning(opt.nfft),opt.noverlap,opt.nfft,'twosided');
-        k = k+1;
-    end
-end
+noise = filtfilt(sos,g, sig);
 
-cohall = median(coh,2);
-f2 = linspace(0,2*pi,size(sig,1)+1)';
-f2 = f2(1:end-1);
-cohall = interp1(f1,cohall, f2, 'linear',0);
+isclip = abs(sig) >= opt.clip;
+noise(isclip) = NaN;
 
-F = fft(sig);
-common = zeros(size(sig));
-iscommon = cohall > opt.coherethresh;
-commonsign = ones(1,nchan);
-for i = 1:nchan
-    common(:,i) = real(ifft(F(:,i).*double(iscommon)));
-    if (i > 1)
-        commonsign(i) = sign(sum(common(:,1).*common(:,i)));
-    end
+noisemag = sqrt(nanmean(noise.^2));
+
+noise = noise ./ repmat(noisemag, [size(sig,1) 1]);
+
+noisesign = ones(1,nchan);
+for i = 2:nchan
+    noisesign(i) = sign(nansum(noise(:,1).*noise(:,i)));
 end
-rem = sig - common;
+noisemag = noisemag.*noisesign;
+noise = bsxfun(@times,noise,noisesign);
+
+noise = nanmean(noise,2);
+
+rem = sig - noise*noisemag;
