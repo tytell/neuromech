123e6c03a3aa39911245a2d5d9e52c3c5af2e7ee
Eric Tytell <Eric.Tytell@tufts.edu>
2014-11-21 11:34:47 -0500

    Checks if file or dataset doesn't exist before reading/writing

--- variables/h5readstruct.m
+++ variables/h5readstruct.m
@@ -1,6 +1,7 @@
 function S = h5readstruct(filename,varargin)
 
 opt.rootgroup = '/';
+opt.structsize = [];
 opt = parsevarargin(opt,varargin,3);
 
 if (isempty(opt.rootgroup))
@@ -14,13 +15,35 @@ else
     end
 end
 
-info = h5info(filename,opt.rootgroup);
-names = {info.Datasets.Name};
+if ~exist(filename,'file')
+    error('h5readstruct:nofile','File does not exist');
+end
 
-C = cell(length(names),1);
+info = h5info(filename,opt.rootgroup);
+
+if ~isempty(opt.structsize)
+    szhi = opt.structsize;
+elseif (~isempty(info.Attributes) && ismember('structsize',{info.Attributes.Name}))
+    szhi = h5readatt(filename,opt.rootgroup,'structsize');
+else
+    szhi = 1;
+end
+names = {info.Datasets.Name};
+szhi = szhi(:)';
+
+C = cell([length(names) szhi]);
 for i = 1:length(names)
     nm1 = [opt.rootgroup names{i}];
-    C{i} = h5read(filename,nm1);
+    D = h5read(filename,nm1);
+    
+    if (numel(szhi) == 1) && (szhi == 1)
+        C{i} = D;
+    else
+        nd = ndims(D);
+        C1 = num2cell(D,1:nd-length(szhi));
+        C1 = reshape(C1,[1 szhi]);
+        C(i,:) = C1(1,:);
+    end
 end
 S = cell2struct(C,names,1);
 
--- variables/h5writestruct.m
+++ variables/h5writestruct.m
@@ -18,6 +18,21 @@ szhi = size(S);
 C = struct2cell(S);
 names = fieldnames(S);
 
+if isempty(S)
+    return;
+end
+    
+if exist(filename,'file')
+    try
+        info = h5info(filename,opt.rootgroup);
+        existnames = {info.Datasets.Name};
+    catch me
+        existnames = {};
+    end
+else
+    existnames = {};
+end
+        
 for i = 1:size(C,1)
     if isnumeric(C{i,1})
         sz1 = size(C{i,1});
@@ -25,11 +40,16 @@ for i = 1:size(C,1)
         D = reshape(D,[sz1 szhi]);
         
         nm1 = [opt.rootgroup names{i}];
-        h5create(filename,nm1, size(D), 'Datatype',class(D));
-        h5write(filename,nm1, D);
+        if ~ismember(names{i},existnames)
+            h5create(filename,nm1, [sz1 Inf(size(szhi))], 'ChunkSize',...
+                [sz1 ones(size(szhi))], 'Datatype',class(D));
+        end
+        h5write(filename,nm1, D, ones(1,length(sz1)+length(szhi)), [sz1 szhi]);
     else
         warning('h5writestruct:type','Skipping field %s because type is not numeric',names{i});
     end
 end
+h5writeatt(filename,opt.rootgroup,'structsize',szhi);
+
         
         
