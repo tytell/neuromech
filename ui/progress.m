d85f87aa3a759a8cabc792eebc2de735ef419fd5
Eric Tytell <Eric.Tytell@tufts.edu>
2015-04-07 13:53:14 -0400

    Added an option to take indices from nested for loops and calculate a total progress

--- ui/progress.m
+++ ui/progress.m
@@ -7,30 +7,46 @@ opt.time = 10;
 opt = parsevarargin(opt,varargin, 4);
 
 global prg_ticstart;
+global prg_totalsteps;
 global prg_numsteps;
 global prev_elap;
+
+if (length(i) > 1) 
+    if ((nargin >= 3) && (length(n) == length(i)))
+        prg_numsteps = n;
+        sz = [1; cumprod(n(1:end-1))];
+    else
+        sz = [1; cumprod(prg_numsteps(1:end-1))];
+    end
+    if all(i == 0)
+        ind = 0;
+    elseif all((i >= 1) & (i <= prg_numsteps))
+        ind = sum((i(:)-1).*sz) + 1;
+    end
+    i = ind;
+end
 
 if (i == 0) || ((i == 1) && (nargin >= 3))
     prg_ticstart = tic;
     fprintf('%s\n', msg);
-    prg_numsteps = n;
+    prg_totalsteps = prod(n);
     prev_elap = 0;
-elseif (i <= prg_numsteps)
+elseif (i <= prg_totalsteps)
     elap = toc(prg_ticstart);
     switch opt.show
         case 'all'
             show = true;
         case 'percent'
-            pctsteps = opt.percent*prg_numsteps;
+            pctsteps = opt.percent*prg_totalsteps;
             show = floor((i-1)/pctsteps) < floor(i/pctsteps);
         case 'time'
             show = elap - prev_elap > opt.time;
     end
     
     if (show || (i <= 1))
-        remain = (elap/i) * (prg_numsteps-i);
+        remain = (elap/i) * (prg_totalsteps-i);
         fprintf('%d/%d steps (%d%%). %s elapsed; %s remaining\n', ...
-            i,prg_numsteps, round((i/prg_numsteps)*100), ...
+            i,prg_totalsteps, round((i/prg_totalsteps)*100), ...
             formatseconds(elap), formatseconds(remain));
     end
 end
